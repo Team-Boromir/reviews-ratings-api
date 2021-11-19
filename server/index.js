b2b245ea6854c52765e038e236fca574bb0283ee
Alice Li <oceanylee@gmail.com>
2021-11-18 22:07:40 -0800

    Create the structure of APIs

--- server/index.js
+++ server/index.js
@@ -1,27 +1,51 @@
 
 const express = require('express');
 const path = require("path");
+const bodyParser = require('body-parser');
+// const db = require('../database');
+
 const port = 3000;
-
 const app = express ();
-
-
-// routers
-const reviews = require('./routes/reviews.js')
 
 //setup bodyparser
 app.use(express.json());
-
-// use the products.js file to handle endpoints that relates to products data
-
-// app.use('/reviews', reviews);
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 // handle root
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+// Get reviews
+app.get('/reviews/:review_id', (req, res) => {
+  res.send('hello world');
+});
+
+// Post reviews
+app.post('/reviews/:review_id', (req, res) => {
+  res.send('hello world');
+});
+
+// Get Meta
+app.get('/reviews/meta/:review_id', (req, res) => {
+  res.send('hello world');
+});
+
+// Report
+app.put('/reviews/:review_id/report', (req, res) => {
+  res.send('hello world');
+});
+
+
+
 app.listen(port, () => {
   console.log(`Server listening at: ${port}`)
 });
+
+
+// API
+
