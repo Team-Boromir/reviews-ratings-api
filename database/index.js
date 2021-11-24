9e788be4c0238a8a7559310f09d27a698f75bbb3
Alice Li <oceanylee@gmail.com>
2021-11-23 23:57:14 -0800

    Reimplement post review query

---
+++ database/index.js
@@ -0,0 +1,108 @@
+const Pool = require('pg').Pool;
+var format = require('pg-format');
+
+const pool = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  password: 'password',
+  database: 'reviews'
+});
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Connected to PostgreSQL!');
+//   }
+// });
+
+// Database Queries
+const getReviewsById = (req, res) => {
+  const product_id = parseInt(req.query.product_id)
+
+  pool.query('SELECT * FROM reviews WHERE product_id = $1', [product_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+const postReviews = (req, res) => {
+  let date = Date.now();
+  const {product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, photos, characteristics} = req.body;
+
+  pool.query('INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id', [product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email], (error, results) => {
+    // console.log(results.rows[0].id);
+    if (error) {
+      throw error;
+    }
+    else {
+      var review_id = results.rows[0].id;
+      var characteristics_list=[];
+      for (var key in characteristics) {
+        characteristics_list.push([key, review_id, characteristics[key]]);
+      }
+
+      // console.log(format('INSERT INTO characteristics_review (characteristics_id,value) VALUES %L', characteristics_list));
+      pool.query(format('INSERT INTO reviews_characteristic_review (characteristic_id, review_id,value) VALUES %L', characteristics_list), [], (error, results) => {
+        if (error) {
+          throw error;
+        } else {
+          var photolist=[];
+          for (const url of photos) {
+            photolist.push([review_id, url]);
+          }
+
+          pool.query(format('INSERT INTO reviews_photos (review_id, url) VALUES %L', photolist), [], (error, results) => {
+            if (error) {
+              throw error;
+            }
+          })
+
+        }
+        res.status(201).json(results.rows);
+      });
+    }
+  })
+
+}
+
+const getMeta = (req, res) => {
+  const product_id = parseInt(req.query.product_id)
+
+  pool.query('SELECT * FROM reviews WHERE product_id = $1', [product_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows);
+    // res.sendStatus(200);
+  })
+}
+
+const markHelpful = (req, res) => {
+  const product_id = parseInt(req.query.product_id)
+
+  pool.query('UPDATE helpfulness SET helpfulness = helpfulness + 1 WHERE product_id = $1' ,[product_id],(error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(204).send(`Helpfull was marked on: ${product_id}`)
+  })
+}
+
+const report = (req, res) => {
+  const product_id = parseInt(req.query.product_id)
+
+  pool.query('UPDATE report SET report = true WHERE product_id = $1' ,[product_id], (error, results)=> {
+    if (error) {
+      throw error
+    }
+    res.status(204).send(`Reported on: ${product_id}`)
+  })
+}
+
+// Don't forget to export your functions!
+module.exports = {
+  getReviewsById, postReviews, getMeta, markHelpful, report
+};
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require("path");
 const bodyParser = require('body-parser');
-// const db = require('../database');
+const db = require('../database/index.js');
 
 const port = 3000;
 const app = express ();
@@ -26,35 +26,31 @@ app.get('/reviews/', (req, res) => {
   const count = req.query.count || 5;
   const sort = req.query.sort;
   const product_id = req.query.product_id;
+  db.getReviewsById(req, res);
   // console.log(page, count, sort, product_id);
 
-  res.sendStatus(200);
+  // res.sendStatus(200);
 });
 
 // Post reviews
 app.post('/reviews/', (req, res) => {
-  const product_id = req.body.product_id;
-  const rating = req.body.rating;
-  const summary = req.body.summary;
-  const body = req.body.body;
-  const recommend = req.body.recommend;
-  const name = req.body.name;
-  const email = req.body.email;
-  const photos = req.body.photos;
-  const characteristics = req.body.characteristics;
 
-  console.log(product_id, rating, summary, body, recommend, name, email, photos, characteristics );
+  db.postReviews(req, res);
 
-  res.sendStatus(201);
+  // console.log(product_id, rating, summary, body, recommend, name, email, photos, characteristics );
+
+  // res.sendStatus(201);
 });
 
 // Get Meta
 app.get('/reviews/meta/', (req, res) => {
   const product_id = req.query.product_id;
 
-  console.log(product_id);
+  db.getMeta(req, res);
 
-  res.sendStatus(200);
+  // console.log(product_id);
+
+  // res.sendStatus(200);
 
 });
 
@@ -62,18 +58,22 @@ app.get('/reviews/meta/', (req, res) => {
 app.put('/reviews/:review_id/helpful', (req, res) => {
   const review_id = req.params.review_id;
 
-  console.log(review_id);
+  db.markHelpful(req, res);
 
-  res.sendStatus(204);
+  // console.log(review_id);
+
+  // res.sendStatus(204);
 });
 
 // Report
 app.put('/reviews/:review_id/report', (req, res) => {
   const review_id = req.params.review_id;
 
-  console.log(review_id);
+  db.report(req, res);
 
-  res.sendStatus(204);
+  // console.log(review_id);
+
+  // res.sendStatus(204);
 });
 
 
